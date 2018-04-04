bea283a1e00ae3a126a91c176fcea721c35cdc5c
junstar96 <junstar96@naver.com>
2018-04-04 15:34:33 +0900

    uml file

--- pycharm/first_study_file.py
+++ pycharm/first_study_file.py
@@ -1,7 +1,32 @@
-import math
+from __future__ import print_function
+from tkinter import *
+from tkinter import font
+import tkinter.messagebox
 
-print(math.log10(100))
-print("file changed")
+window = Tk()
+def print_e(name):
+    print(name)
 
-d = 
-for k,v in
+def pros():
+    dollar = float(e1.get())
+    e2.insert(0, str(dollar*1200))
+
+
+l1 = Label(window, text = "hello")
+l2 = Label(window, text = "world")
+l1.grid(row = 0, column = 0)
+l2.grid(row = 1, column = 0)
+
+
+e1 = Entry(window)
+e2 = Entry(window)
+e1.grid(row = 0, column = 1)
+e2.grid(row = 1, column = 1)
+
+
+
+button = Button(window, text="클릭!", command=pros) #이러면 주석이
+
+button.grid(row = 2, column = 0)
+
+window.mainloop()
